fdd86192651260647c4ca7611324c3eca85a32ed
John_Galt <john.galt.prog@gmail.com>
2023-04-12 12:14:50 +0300

    create Portal in React

---
+++ src/BootstrapTest.js
@@ -0,0 +1,20 @@
+import { Container, Row, Col, Carousel, Form, Button } from "react-bootstrap";
+
+const BootstrapTest = (props) => {
+    return (
+        <Container className="mt-5 mb-5">
+            <Row>
+                <Col>
+                    {props.left}
+                </Col>
+                <Col>
+                    {props.right}
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default BootstrapTest;
+
+
